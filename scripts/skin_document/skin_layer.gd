bf655dadda0749fabde55dec84b276a1d6db2ef1
Toby Yang <tianer2820@163.com>
2022-01-03 10:50:45 -0500

    add layer mode enum, add copy from function

--- scripts/skin_document/skin_layer.gd
+++ scripts/skin_document/skin_layer.gd
@@ -6,6 +6,12 @@ var name: String
 var visible: bool
 var alpha: float
 var image: Image
+var mode
+
+
+enum{
+	NORMAL,
+}
 
 
 func _init(name: String, size: Vector2) -> void:
@@ -15,11 +21,18 @@ func _init(name: String, size: Vector2) -> void:
 	image = Image.new()
 	image.create(size.x, size.y, false, Image.FORMAT_RGBA8)
 	image.fill(Color(0, 0, 0, 0))
+	mode = NORMAL
 
+func copy_from(other):
+	other = other as SkinLayer
+	name = other.name
+	visible = other.visible
+	alpha = other.alpha
+	image.copy_from(other.image)
+	mode = other.mode
 
 func duplicate():
 	var skin_layer_class = load("res://scripts/skin_document/skin_layer.gd")
 	var dup = skin_layer_class.new(name, image.get_size())
-	dup.alpha = alpha
-	dup.image.copy_from(image)
+	dup.copy_from(self)
 	return dup
