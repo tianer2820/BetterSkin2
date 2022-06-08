2fda61b5a677758a6c3f3349482046e47f48d05a
HitCoder9768 <hitcoder9768@gmail.com>
2022-06-08 20:26:26 +0100

    Hook up toolstrip buttons to functions

--- assets/gui_root.gd
+++ assets/gui_root.gd
@@ -9,3 +9,18 @@ extends PanelContainer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+
+
+func _on_ButtonNew_pressed():
+	DocumentManager.ask_create_new_skin()
+	pass # Replace with function body.
+
+
+func _on_ButtonOpen_pressed():
+	DocumentManager.ask_open_skin()
+	pass # Replace with function body.
+
+
+func _on_ButtonSave_pressed():
+	DocumentManager.ask_save_skin()
+	pass # Replace with function body.
