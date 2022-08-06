570549c16ef14c5fe170cca367589b022196e906
tianer2820 <tianer2820@outlook.com>
2022-08-06 17:13:55 -0400

    fix index out of bound error in test tool, add more test props

--- scripts/tools/test_tool.gd
+++ scripts/tools/test_tool.gd
@@ -12,7 +12,10 @@ func _init() -> void:
 	
 	displayed_props = {
 		"test_int": int_prop(3, 0, 6, 1),
-		"test_float": float_prop(1.234, -2, 2, 0.3),
+		"test_float": float_prop(1.234, -2, 2),
+		"test_tip": brush_tip_prop(ToolProp.new()),
+		"test_bool": bool_prop(false),
+		"test_choice": choice_prop(0, ['aaa', 'bbb', 'ccc']),
 	}
 	
 func duplicate():
@@ -40,7 +43,12 @@ func pen_move(uv: Vector2):
 func _draw_pixel(uv):
 	var layer = DocumentManager.draw_buffer_layer
 	var img = layer.image
-	img.lock()
-	img.set_pixelv(uv, ToolManager.get_prime_color())
-	img.unlock()
-	DocumentManager.queue_render_skin()
+	
+	var img_size = img.get_size()
+	var img_rect = Rect2(0, 0, img_size.x, img_size.y)
+	
+	if img_rect.has_point(uv):
+		img.lock()
+		img.set_pixelv(uv, ToolManager.get_prime_color())
+		img.unlock()
+		DocumentManager.queue_render_skin()
